ef054652dfb13e4d564aed4210fcc803ca478dcc
sardor19639 <36393250+sardor19639@users.noreply.github.com>
2020-05-17 18:36:32 +0600

    Deleted not used code

--- src/test/java/com/online/school/OnlineSchoolTests.java
+++ src/test/java/com/online/school/OnlineSchoolTests.java
@@ -1,8 +1,6 @@
 package com.online.school;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.beans.factory.annotation.Autowired;
